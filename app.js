071d0f87ecfca44bd1caa93a3a16d2b5f5583ce5
Matt Yard <mattyard11@gmail.com>
2022-01-21 15:30:26 -0500

    Connects database and creates User and Page models

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const layout = require('./views/layout')
+const { db, Page, User } = require('./models')
+
+const PORT = 1338;
+
+const init = async () => {
+  await Page.sync({force: true})
+  await User.sync({force: true})
+
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  });
+}
+
+init()
 
 app.use(morgan("dev"));
 
@@ -13,8 +27,3 @@ app.get("/", (req, res) => {
   res.send(layout(''));
 })
 
-const PORT = 1338;
-
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
